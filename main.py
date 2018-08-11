8b8f67fe97cb1dc31bdc64db83ec390de57bc6aa
dinsizimansiz <oguzvuruskaner@gmail.com>
2018-08-12 02:44:22 +0300

    Steps per epoch changed.

--- main.py
+++ main.py
@@ -79,7 +79,7 @@ def main(args=None):
 					  optimizer=optimizer,
 					  metrics=['acc'])
 
-		model.fit_generator(train_generator, steps_per_epoch=2, epochs=300, validation_data=eval_generator,
+		model.fit_generator(train_generator, steps_per_epoch=200, epochs=300, validation_data=eval_generator,
 							validation_steps=20, callbacks=[pushToGitCallback,modelCheckpoint])#early_stopper, lr_reducer, modelCheckpoint
 	
 		
